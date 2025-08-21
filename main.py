55a27f73d6975d83700e2093adfe4d7ea8a3d8fe
Vedant Vashishtha <aiml202271@global.org.in>
2025-08-21 09:21:08 +0530

    Basic and most used function of Streamlit

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hello Welcome to my first Website")
+st.subheader("This is made using streamlit and python")
+st.text("I Love Python")
+st.write("It is just a simple written text using write")
